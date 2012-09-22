dca7d5f7f5bd3878375a8963e59f193ae0cf4641
Aaron Kling <akling8467@students.pcci.edu>
2012-09-21 21:55:32 -0500

    Stuff I'm working on.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,72 +14,63 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         lbl_error_message.Text = tm_EmptyString;
+        System.Web.Security.MembershipUserCollection mutest;
+        /*mutest = System.Web.Security.Membership.GetAllUsers();
+        foreach (System.Web.Security.MembershipUser muser in mutest)
+        {
+            lbl_error_message.Text += muser.UserName + "<br />";
+        }*/
+        /*System.Web.Security.MembershipUser mtest;
+        mtest = System.Web.Security.Membership.GetUser("studentnet\\107088");
+        lbl_error_message.Text = (mtest == null ? "Not Found" : mtest.UserName) + "<br />";*/
     }
     
     protected void LoginButtonClick(object sender, EventArgs e)
     {
         Int32 intUserType = 0;
-        using (DirectoryEntry entry = new DirectoryEntry())
-        {
-            entry.Username = txt_username.Text;
-            entry.Password = txt_password.Text;
-
-            DirectorySearcher searcher = new DirectorySearcher(entry);
-
-            searcher.Filter = "(objectclass=user)";
-
-            try
-            {
-                searcher.FindOne();
-            }
-            catch (DirectoryServicesCOMException ex)
-            {
-                if (ex.ErrorCode == -2147023570) // Login or password is incorrect
-                {
-                    lbl_error_message.Text = "Username or password was invalid.<br />";
-                }
-                return;
-            }
-        }
-
-        // Open DB connection
-        SqlConnection TM_DB = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["SEI_TMConnString"].ConnectionString);
-        TM_DB.Open();
-
-        SqlCommand command_GetUser = new SqlCommand("tm_GetUser", TM_DB);
-        command_GetUser.CommandType = CommandType.StoredProcedure;
-        command_GetUser.Parameters.AddWithValue("@UserID", txt_username.Text);
-
-        SqlDataReader user_reader = command_GetUser.ExecuteReader();
-
-        //Get the first user returned
-        if (user_reader.HasRows)
-        {
-            while (user_reader.Read())
-            {
-                intUserType = (Int32)user_reader["TypeID"];
-                break;
-            }
-        }
+        if (!System.Web.Security.Membership.ValidateUser(txt_username.Text, txt_password.Text))
+            lbl_error_message.Text += "Username or password was invalid.<br />";
         else
         {
-            lbl_error_message.Text = "You are not a current user.  Please contact the administrator of Time Machine.<br />";
-        }
-        user_reader.Close();
-        user_reader.Dispose();
-        command_GetUser.Dispose();
+            // Open DB connection
+            SqlConnection TM_DB = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["SEI_TMConnString"].ConnectionString);
+            TM_DB.Open();
 
-        // Close DB connection
-        TM_DB.Close();
-        TM_DB.Dispose();
+            SqlCommand command_GetUser = new SqlCommand("tm_GetUser", TM_DB);
+            command_GetUser.CommandType = CommandType.StoredProcedure;
+            command_GetUser.Parameters.AddWithValue("@UserID", txt_username.Text);
 
-        if (lbl_error_message.Text == tm_EmptyString)
-        {
-            Session["s_user"] = txt_username.Text;
-            if (intUserType >= 1)
-                Response.Redirect("Statistics.aspx");
+            SqlDataReader user_reader = command_GetUser.ExecuteReader();
+
+            //Get the first user returned
+            if (user_reader.HasRows)
+            {
+                while (user_reader.Read())
+                {
+                    intUserType = (Int32)user_reader["TypeID"];
+                    break;
+                }
+            }
             else
-                Response.Redirect("Timelog.aspx");
+            {
+                lbl_error_message.Text = "You are not a current user.  Please contact the administrator of Time Machine.<br />";
+            }
+            user_reader.Close();
+            user_reader.Dispose();
+            command_GetUser.Dispose();
+
+            // Close DB connection
+            TM_DB.Close();
+            TM_DB.Dispose();
+
+            if (lbl_error_message.Text == tm_EmptyString)
+            {
+                Session["s_user"] = txt_username.Text;
+                if (intUserType >= 1)
+                    Response.Redirect("Statistics.aspx");
+                else
+                    Response.Redirect("Timelog.aspx");
+            }
         }
     }
 }
