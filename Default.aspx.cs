eec2ba6122f23c5d983a3d511bcf290b89125c31
Aaron Kling <akling8467@students.pcci.edu>
2012-09-24 19:06:17 -0500

    Revert to older style login page.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,15 +14,6 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         lbl_error_message.Text = tm_EmptyString;
-        System.Web.Security.MembershipUserCollection mutest;
-        /*mutest = System.Web.Security.Membership.GetAllUsers();
-        foreach (System.Web.Security.MembershipUser muser in mutest)
-        {
-            lbl_error_message.Text += muser.UserName + "<br />";
-        }*/
-        /*System.Web.Security.MembershipUser mtest;
-        mtest = System.Web.Security.Membership.GetUser("studentnet\\107088");
-        lbl_error_message.Text = (mtest == null ? "Not Found" : mtest.UserName) + "<br />";*/
     }
     
     protected void LoginButtonClick(object sender, EventArgs e)
@@ -30,9 +21,30 @@ public partial class _Default : System.Web.UI.Page
         Int32 intUserType = 0;
         Boolean userDisabled = false;
 
-        if (!System.Web.Security.Membership.ValidateUser(txt_username.Text, txt_password.Text))
-            lbl_error_message.Text += "Username or password was invalid.<br />";
-        else
+        using (DirectoryEntry entry = new DirectoryEntry())
+        {
+            entry.Username = txt_username.Text;
+            entry.Password = txt_password.Text;
+
+            DirectorySearcher searcher = new DirectorySearcher(entry);
+
+            searcher.Filter = "(objectclass=user)";
+
+            try
+            {
+                searcher.FindOne();
+            }
+            catch (DirectoryServicesCOMException ex)
+            {
+                if (ex.ErrorCode == -2147023570) // Login or password is incorrect
+                {
+                    lbl_error_message.Text = "Username or password was invalid.<br />";
+                }
+                return;
+            }
+        }
+
+        if (lbl_error_message.Text == tm_EmptyString)
         {
             // Open DB connection
             SqlConnection TM_DB = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["SEI_TMConnString"].ConnectionString);
